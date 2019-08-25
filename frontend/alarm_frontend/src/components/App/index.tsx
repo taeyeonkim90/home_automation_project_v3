023849c205db88cc7e44c742fe667b7af02b07dc
Andrew Kim <taeyeonkim90@gmail.com>
2019-08-25 15:26:18 -0700

    added material-ui to frontend
    added initial app bar component

---
+++ frontend/alarm_frontend/src/components/App/index.tsx
@@ -0,0 +1,17 @@
+import 'typeface-roboto';
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+import AppBar from '../AppBar';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <AppBar></AppBar>
+    </div>
+  );
+};
+
+export default App;
